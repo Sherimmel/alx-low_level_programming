4b8c347ac3d3b6b5ba61faa2f86340cc475b4c66
Sheridan Larbi <sheridanlarbi@yahoo.com>
2022-09-08 13:54:09 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print the size of many types
+ *
+ * Return: Always 0 (Success)
+ */
+init main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d byte(s)\n",size of(a));
+	printf("size of an int: %d byte(s)\n", size of (b));
+	printf("size of a long int: %d  byte(s)\n", size of (c));
+	printf("Size of a long long int: %d byte(s)\n", size of (d));
+	printf("Size of a float: %d byte(s)\n", size of (e));
+	return(0)
+}
