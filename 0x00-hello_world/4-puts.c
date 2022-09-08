d4f22c69a0d401b8d1a1e522ab7b0f9fe3f5c5b6
Sheridan Larbi <sheridanlarbi@yahoo.com>
2022-09-08 11:28:23 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print c
+ *
+ *  Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like buiding a multilingual puzzle");
+	return (0);
+}
