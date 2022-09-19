96ee06400d35d675b8f34bc3455e4512ef877d51
Sherimmel <110994311+Sherimmel@users.noreply.github.com>
2022-09-19 14:51:53 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,53 +1,27 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
-
 #include "main.h"
 
-
-
 /**
+ * main - entry point
  *
- *  * main - entry point
- *
- *   *
- *
- *    * Return: generated password
- *
- *     */
-
-
+ * Return: generated password
+ */
 
 int main(void)
-
 {
+	char c;
+	int x;
 
-		char c;
+	srand(time(0));
+	while (x <= 2645)
+	{
+		c = rand() % 128;
+		x += c;
+		putchar(c);
+	}
+	putchar(2772 - x);
 
-			int x;
-
-
-
-				srand(time(0));
-
-					while (x <= 2645)
-
-							{
-
-										c = rand() % 128;
-
-												x += c;
-
-														putchar(c);
-
-															}
-
-						putchar(2772 - x);
-
-
-
-							return (0);
-
+	return (0);
 }
