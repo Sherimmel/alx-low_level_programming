05eea914ff76a7c0a88e2bcdf00abf14c6b93c8a
Sherimmel <110994311+Sherimmel@users.noreply.github.com>
2022-09-19 14:37:31 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,127 +1,22 @@
 #include <stdio.h>
-
 #include "main.h"
 
-
-
 /**
+ * puts2 - prints every other character
+ * @str: string
  *
- *  * puts2 - prints every other character
- *
- *   * @str: string
- *
- *    *
- *
- *     * Return: nothing
- *
- *      */
-
-
+ * Return: nothing
+ */
 
 void puts2(char *str)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			while (*(str + i) != '\0')
-
-					{
-
-								if (i % 2 == 0)
-
-												putchar(*(str + i));
-
-										i++;
-
-											}
-
-				putchar(10);
-
-}#include <stdio.h>
-
-#include "main.h"
-
-
-
-/**
- *
- *  * puts2 - prints every other character
- *
- *   * @str: string
- *
- *    *
- *
- *     * Return: nothing
- *
- *      */
-
-
-
-void puts2(char *str)
-
-{
-
-		int i = 0;
-
-
-
-			while (*(str + i) != '\0')
-
-					{
-
-								if (i % 2 == 0)
-
-												putchar(*(str + i));
-
-										i++;
-
-											}
-
-				putchar(10);
-
-}i#include <stdio.h>
-
-#include "main.h"
-
-
-
-/**
- *
- *  * puts2 - prints every other character
- *
- *   * @str: string
- *
- *    *
- *
- *     * Return: nothing
- *
- *      */
-
-
-
-void puts2(char *str)
-
-{
-
-		int i = 0;
-
-
-
-			while (*(str + i) != '\0')
-
-					{
-
-								if (i % 2 == 0)
-
-												putchar(*(str + i));
-
-										i++;
-
-											}
-
-				putchar(10);
-
+	while (*(str + i) != '\0')
+	{
+		if (i % 2 == 0)
+			putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
 }
